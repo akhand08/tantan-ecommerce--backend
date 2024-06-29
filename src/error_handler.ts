3bcefa4d610e425c813d4d96748d18289923399f
akhand08 <adib08@outlook.com>
2024-06-29 23:08:14 +0600

    Generic error handler has been added

---
+++ src/error_handler.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express'
+import { ErrorCode, HttpException } from './exceptions/root';
+import { InternalException } from './exceptions/exception_handle';
+
+export const errorHandler = (method: Function) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        try{
+
+            method(req, res, next);
+
+        }catch(error:any) {
+            
+            let exception: HttpException;
+
+            if(error instanceof HttpException) {
+                exception = error
+            }
+            else {
+                exception = new InternalException("Something went wrong", ErrorCode.INTERNAL_EXCEPTION, error)
+            }
+            next(exception);
+
+        }
+    }
+}
