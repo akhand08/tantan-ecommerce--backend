0a1df87652b3c5721891e0d1b255a38d36edc8e1
akhand08 <adib08@outlook.com>
2024-07-04 18:17:26 +0600

    Routes and Schema for products have also beed added

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import { errorHandler } from "../error_handler";
-import { createProduct } from "../controllers/products";
+import { createProduct, deleteProduct, getProductById, listProducts, updateProduct } from "../controllers/products";
 import { authMiddleware } from "../middlewares/auth";
+import { adminMiddleware } from "../middlewares/admin";
 
 
 const productRoutes: Router = Router();
 
 
-productRoutes.post('/', [authMiddleware] errorHandler(createProduct))
+productRoutes.post('/',[authMiddleware, adminMiddleware],  errorHandler(createProduct));
+productRoutes.put('/:id', [authMiddleware, adminMiddleware],  errorHandler(updateProduct));
+productRoutes.delete('/:id', [authMiddleware, adminMiddleware],  errorHandler(deleteProduct));
+productRoutes.get('/', [authMiddleware, adminMiddleware],  errorHandler(listProducts));
+productRoutes.get('/:id', [authMiddleware, adminMiddleware],  errorHandler(getProductById));
+
+
 
 export default productRoutes;
--- src/schema/products.ts
+++ src/schema/products.ts
@@ -4,8 +4,8 @@ import {z} from 'zod'
 export const ProductSchema = z.object({
 
     name: z.string(),
-    description: z.string().min(100).max(300),
+    description: z.string().min(10).max(200),
     price: z.number().positive().finite().multipleOf(0.01),
-    tags: z.string()
+    tags: z.array(z.string())
 
 })
