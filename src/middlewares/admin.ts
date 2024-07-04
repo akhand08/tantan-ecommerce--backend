b95bdd33fd1a7bd10a17ae9c87fed5736c922c6e
akhand08 <adib08@outlook.com>
2024-07-04 16:18:41 +0600

    Admin middleware has been added

---
+++ src/middlewares/admin.ts
@@ -0,0 +1,18 @@
+import { User } from "@prisma/client";
+import { NextFunction, Request, Response } from "express";
+import { UnauthorizedException } from "../exceptions/exception_handle";
+import { ErrorCode } from "../exceptions/root";
+
+
+
+export const adminMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+
+    const user = req.user as any;
+    if(user.role == 'ADMIN') {
+        next();
+    }
+    else {
+        throw new UnauthorizedException("Unauthorized", ErrorCode.Unauthorized, null)
+    }
+
+}
